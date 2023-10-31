2f6d732fe55eee286a4207fc02905da4b7cdd079
Nicola Fontana <ntd@entidi.it>
2023-10-31 10:19:21 +0100

    Drop pthread dependency
    This is a demo program, so simpler is always better.

--- src/main.c
+++ src/main.c
@@ -1,27 +1,12 @@
-#define _DEFAULT_SOURCE /* needed for usleep() */
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-
 #include "lv_drivers/wayland/wayland.h"
 #include "lvgl/examples/lv_examples.h"
 
+#include <unistd.h>
 
-static void *
-tick_thread(void *data)
-{
-    (void) data;
-    while (1) {
-        usleep(5 * 1000);
-        lv_tick_inc(5); /*Tell LittelvGL that 5 milliseconds were elapsed*/
-    }
-    return NULL;
-}
 
 int
 main(int argc, char **argv)
 {
-    pthread_t thread1;
     lv_disp_t *disp;
 
     LV_UNUSED(argc);
@@ -34,18 +19,14 @@ main(int argc, char **argv)
                                     WAYLAND_SURF_TITLE, NULL);
     LV_UNUSED(disp);
 
-    pthread_create(&thread1, NULL, tick_thread, NULL);
-
     lv_example_get_started_1();
 
     while (1) {
-        /* Periodically call the lv_task handler.
-         * It could be done in a timer interrupt or an OS task too.*/
         lv_timer_handler();
         usleep(5 * 1000);
+        lv_tick_inc(5);
     }
 
-    pthread_join(thread1, NULL);
     lv_wayland_deinit();
     lv_deinit();
 
